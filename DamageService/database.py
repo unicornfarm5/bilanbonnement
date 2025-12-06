5fc4f97e80d5e8e9d96a9cb1121dc774a5e93b24
Dr. Clocktor <jonas@sakejo.dk>
2025-12-06 14:58:46 +0100

    lille tilføjelse

--- DamageService/database.py
+++ DamageService/database.py
@@ -13,4 +13,35 @@ def init_db():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    cursor.execute
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS damage_levels (
+                   id INT PRIMARY KEY,
+                   level_name TEXT UNIQUE NOT NULL,
+                   price INT NOT NULL
+     )
+ ''')
+    
+    levels = [
+        ('ingen', 0),
+        ('let', 500),
+        ('middel', 1500),
+        ('svær', 3000),
+        ('kritisk', 5000)
+    ]
+    # Forklar hvad der sker her
+    cursor.executemany('INSERT OR IGNORE INTO damage_levels (level_name, price) VALUES (?, ?)', levels) 
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS damage (
+                   damage_report_id INT PRIMARY KEY AUTOINCREMENT,
+                   damage_level_id INTEGER NOT NULL, -- Peger på niveauet af skaden
+                   damage_description TEXT,
+                   damage_price INT,
+                   licensplate TEXT, --mangler foreign key, men hvad menes der?
+                   order_id INT,
+                   FOREIGN KEY (damage_level_id) REFERENCES damage_levels(id) --må kun indeholde ID'er der eksisterer i damage_levels tabel
+                   created_at (DATETIME DEFAULT current_timestamp)
+                   )
+                ''')
+    conn.commit()
+    conn.close()
